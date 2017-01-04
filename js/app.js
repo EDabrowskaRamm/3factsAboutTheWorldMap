8e6b332763c113e3d8a68f77420643f48a305c71
Edyta Dąbrowska-Ramm <editt.dabrowska@gmail.com>
2017-01-04 21:41:04 +0100

    version5

--- js/app.js
+++ js/app.js
@@ -1,12 +1,46 @@
 $(function(){
+//navigation drop down
   var $navContainer = $('.navigation');
   var $headerBars = $navContainer.find('i');
   var $navigation = $navContainer.find('nav');
 
-//navigation drop down
   $headerBars.on('click', function(){
     $navigation.toggle();
   });
+
+//country highlight
+
+  var SVGcontainer = document.getElementById('mySvg');
+
+  SVGcontainer.onload = function(){
+    var lands = SVGcontainer.querySelectorAll('.land');
+
+    for(var i = 0, len = lands.length; i < len; i++){
+      lands[i].addEventListener('mouseenter', function(){
+        this.style.fill = '#EF5159';
+      })
+
+      lands[i].addEventListener('click', function(){
+        console.log('clicked' + i);
+        this.style.scale(2, 2);
+      })
+    }
+
+    for(var i = 0, len = lands.length; i < len; i++){
+      lands[i].addEventListener('mouseout', function(){
+        this.style.fill = '#CCCCCC';
+      })
+    }
+  };
+
+//clicked country gets bigger
+
+
+
+
+
+
+
 
 
 });
